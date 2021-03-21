3a5e29d92aa1cf35e41dbad33760968771b9c5c8
ziyanfadilla <ziyanfadilla1@gmail.com>
2021-03-21 10:18:25 +0700

    18090058

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,65 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    Nama : Ziyan Fadilla Saporette
+    NIM : 18090058
+    Kelas : 6B
+</body>
+</html>
+ --}}
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+      <div class="container">
+          <h2><strong>INTRODUCE</strong></h2>
+          <a href="" type="btn" class="btn btn-primary" style="margin-top: 30px">Tambah Data</a>
+          <table class="table table-striped" style="margin-top: 20px; text-align: center">
+            <thead>
+              <tr style="">
+                <th scope="col">No</th>
+                <th scope="col">NIM</th>
+                <th scope="col">NAMA</th>
+                <th scope="col">KELAS</th>
+                <th scope="col">AKSI</th>
+
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>18090058</td>
+                <td>Ziyan Fadilla Saporette</td>
+                <td>6B</td>
+                <td>
+                    <a href="" type="btn" class="btn btn-warning">Ubah</a>
+                    <a href="" type="btn" class="btn btn-danger">Hapus</a>
+                </td>
+              </tr> 
+            </tbody>
+          </table>
+      </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
